583da3c4282644150e1eb26d2635787ad495bd39
Craig J. Bass <craig@madetech.com>
2025-05-26 13:28:03 +0000

    Remove unused variables; reformat

--- HouseSolarPredictor/Planning/TimeSegment.cs
+++ HouseSolarPredictor/Planning/TimeSegment.cs
@@ -27,29 +27,19 @@ public class TimeSegment
 
     public Gbp Cost()
     {
-        var segment = this;
-        var gridCost = Gbp.Zero;
-        decimal wastedSolarCost = 0;
-        
-        var solarUsed = Time.Kwh.Min(segment.ExpectedSolarGeneration, segment.ActualGridUsage);
+        var solarUsed = Time.Kwh.Min(ExpectedSolarGeneration, ActualGridUsage);
         var batteryContribution = Time.Kwh.Zero;
         
-        if (segment.Mode == OutputsMode.Discharge)
-        {
-            batteryContribution = segment.StartBatteryChargeKwh - segment.EndBatteryChargeKwh;
-        }
-        
-        // Calculate grid electricity used (load - solar - battery)
-        var gridUsed = segment.ActualGridUsage - solarUsed - batteryContribution;
+        if (Mode == OutputsMode.Discharge)
+            batteryContribution = StartBatteryChargeKwh - EndBatteryChargeKwh;
+                
+        var gridUsed = ActualGridUsage - solarUsed - batteryContribution;
         if (gridUsed.Value < 0)
             gridUsed = Kwh.Zero;
             
-        // Calculate grid cost using the Kwh * ElectricityRate operator
-        gridCost = gridUsed * segment.GridPrice;
-        
-        // Calculate wasted solar cost using the Kwh * ElectricityRate operator
-        var wastedSolar = segment.WastedSolarGeneration;
-        var wastedSolarCostGbp = wastedSolar * segment.GridPrice;
+        var gridCost = gridUsed * GridPrice;
+        var wastedSolar = WastedSolarGeneration;
+        var wastedSolarCostGbp = wastedSolar * GridPrice;
         
         return gridCost + wastedSolarCostGbp;
     }
